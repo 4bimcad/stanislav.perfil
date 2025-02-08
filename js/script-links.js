5098fe36f62f44079430639d299d881b6cae788a
Stanislav Tsvitkov <4bimcad@gmail.com>
2025-02-08 05:53:16 -0500

    Update script-links.js

--- js/script-links.js
+++ js/script-links.js
@@ -5,12 +5,12 @@ function toggleResumes() {
 
     if (!resumeContainer.innerHTML.trim()) { 
         let password = prompt("Enter Password:");
-        if (password === "arequipa") { 
+        if (password === "demo") { 
             resumeContainer.innerHTML = `
-                <div id="resumeLinks" class="flex flex-col space-y-1">
-                    <a href="engineer.html" class="text-blue-600 hover:underline">Engineer CV [EN]</a>
-                    <a href="ingeniero.html" class="text-blue-600 hover:underline">Ingeniero CV [ES]</a>
-                </div>`;
+                <div id="resumeLinks" class="flex flex-col space-y-2 bg-gray-100 p-4 rounded-lg shadow-md max-w-xs mx-auto">
+    <a href="engineer.html" class="text-blue-600 hover:underline hover:text-blue-800 font-semibold text-lg transition duration-200">Engineer CV [EN]</a>
+    <a href="ingeniero.html" class="text-blue-600 hover:underline hover:text-blue-800 font-semibold text-lg transition duration-200">Ingeniero CV [ES]</a>
+</div>`;
             resumeText.innerText = "Hide Info";
             eyeIcon.innerHTML = `
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
